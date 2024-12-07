984939d0efb8d7da9507765b83f5fa79ec34a7b6
Nilanshu Garhewal <nilanshugarhewal1@gmail.com>
2024-12-07 13:07:49 +0530

    newly added database file *don't touch them

---
+++ app.js
@@ -0,0 +1,38 @@
+const { faker } = require("@faker-js/faker");
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "chatbot",
+  password: "Sqlinobj?(no)",
+});
+
+// Randome user data hello
+let getRandomUser = () => {
+  return [
+    faker.string.uuid(),
+    faker.internet.username(),
+    faker.internet.email(),
+    faker.internet.password(),
+  ];
+};
+
+let query = "INSERT INTO users (user_id, username, email, password) VALUES ?";
+
+let data = [];
+
+for (let i = 1; i <= 100; i++) {
+  data.push(getRandomUser());
+}
+
+try {
+  connection.query(query, [data], (err, res) => {
+    if (err) throw err;
+    console.log(res);
+  });
+} catch (err) {
+  console.log(err);
+}
+
+connection.end();
---
+++ schema.sql
@@ -0,0 +1,7 @@
+-- SQLBook: Code
+CREATE TABLE users (
+    user_id VARCHAR(50),
+    username VARCHAR(50),
+    email VARCHAR(100),
+    password VARCHAR(50)
+)
